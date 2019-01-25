00b3678e4801b7782cc95a52906dfe6cdaeebabc
stackprobe <stackprobes@gmail.com>
2019-01-26 00:45:16 +0900

    coffee break

--- HTTDir/HTTDir/BootTools.cs
+++ HTTDir/HTTDir/BootTools.cs
@@ -6,6 +6,8 @@ using System.IO;
 
 namespace HTTDir
 {
+	// sync > @ BootTools
+
 	public class BootTools
 	{
 		public static string SelfFile;
@@ -19,4 +21,6 @@ namespace HTTDir
 			Directory.SetCurrentDirectory(SelfDir);
 		}
 	}
+
+	// < sync
 }
