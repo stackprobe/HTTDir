fded8b38cca0c71444a81b3f35e8340f0917c965
stackprobe <stackprobes@gmail.com>
2020-08-18 20:36:57 +0900

    Tomorrow

--- AutoRelease.bat
+++ AutoRelease.bat
@@ -1,23 +1,16 @@
 @rem このバッチファイルは C:\Factory\Resource\AutoRelease.bat_template.txt と同じ内容でなければなりません。
 @rem 固有の記述はタブスペースで字下げして下さい。
-@rem 連続する固有の記述行の直後に空行を入れなければなりません。
+@rem 固有の記述行の直後にある１つ以上連続する空行は無視されます。
 
 CLS
 rem リリースして qrum します。
-PAUSE
+IF NOT "%1" == "/-P" PAUSE
 
 CALL newcsrr
 
 CALL ff
 cx **
 CD /D %~dp0.
-
-	CD ..\HTT
-
-	CALL qq
-	cx **
-
-	CD /D %~dp0.
 
 CALL qq
 cx **
