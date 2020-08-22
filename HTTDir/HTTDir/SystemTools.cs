d29688219d893f6dd269130aa50793ca5cfa8564
stackprobe <stackprobes@gmail.com>
2020-08-22 17:48:19 +0900

    Nix

--- HTTDir/HTTDir/SystemTools.cs
+++ HTTDir/HTTDir/SystemTools.cs
@@ -166,11 +166,11 @@ namespace HTTDir
 					{
 						ContextMenuStrip menu = new ContextMenuStrip();
 
-#if true
 						{
 							ToolStripMenuItem item = new ToolStripMenuItem();
 
-							item.Text = "内容をコピー";
+#if false
+							item.Text = "内容をクリップボードにコピー";
 							item.Click += (sdr, ev) =>
 							{
 								try
@@ -180,19 +180,13 @@ namespace HTTDir
 								catch
 								{ }
 							};
-
-							menu.Items.Add(item);
-						}
 #else
-						{
-							ToolStripMenuItem item = new ToolStripMenuItem();
-
 							item.Text = "項目なし";
 							item.Enabled = false;
+#endif
 
 							menu.Items.Add(item);
 						}
-#endif
 
 						c.ContextMenuStrip = menu;
 					}
